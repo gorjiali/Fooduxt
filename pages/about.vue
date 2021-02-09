f0e6a16024a428eafc287d8e62a04eb4d2320427
Ali Gorji <ag.aligorji@gmail.com>
2021-02-09 16:58:55 +0330

    project initial with nuxt-create-app

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>hi from about page!</h2>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="container">
+    <h2>Hi from home page!</h2>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
